87cb975ae363b1067e2869ae26473d747a697830
Gabriel <111046337+TheNotoriousGB@users.noreply.github.com>
2024-11-18 09:19:36 +0100

    Asks the user for date, closes #57

--- helper.py
+++ helper.py
@@ -1,26 +1,27 @@
 from dataclasses import dataclass
+import datetime
 
 items = []
-
 
 @dataclass
 class Item:
     text: str
+    date: datetime.datetime
     isCompleted: bool = False
 
-
-def add(text):
+def add(text, date):
     text = text.replace('b', 'bbb').replace('B', 'Bbb')
-    items.append(Item(text))
-
+    # Datum parsen
+    date = datetime.datetime.strptime(date, '%Y-%m-%d')
+    items.append(Item(text, date))
 
 def get_all():
     return items
 
-
 def get(index):
     return items[index]
 
-
 def update(index):
-    items[index].isCompleted = not items[index].isCompleted
+    # Hier sicherstellen, dass wir auf das richtige Item zugreifen
+    item = items[index]  # Hier holen wir das Item
+    item.isCompleted = not item.isCompleted  # Wir ändern den Status des Items
